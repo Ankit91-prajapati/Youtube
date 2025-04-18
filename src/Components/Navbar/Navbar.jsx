79817d6ccc32b22cbb2c28315536ec44760cba60
Ankit91-prajapati <ankitprajapati6458@gmail.com>
2025-04-18 10:03:12 +0530

    updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = ({setSidebar}) => {
 
       <div className="nav-middle flex-div">
         <div className="search-box flex-div">
-          <input type="text" placeholder="Search" />
+          <input id = "box" type="text" placeholder="Search" />
           <img src={search_icon} alt="" />
         </div>
       </div>
